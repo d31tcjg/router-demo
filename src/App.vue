2b374e49b28fd3ea00b6e30074649bcdfdd08a9c
Cesar Garcia <d31tcjg@gmail.com>
2021-02-23 15:58:25 -0600

    Final Commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/news">News</router-link> |
+    <router-link to="/weather">Weather</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<script setup></script>
